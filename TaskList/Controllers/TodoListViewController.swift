e136eecfb4d90fd100b8c912e392a84c364e7148
Waga <wagaodongo@gmail.com>
2019-09-19 09:42:58 +0300

    convert to mvc

--- TaskList/Controllers/TodoListViewController.swift
+++ TaskList/Controllers/TodoListViewController.swift
@@ -30,9 +30,9 @@ class TodoListViewController: UITableViewController {
         itemArray.append(newItem3)
         
         
-//        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-//            itemArray = items
-//        }
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+            itemArray = items
+        }
     }
     
     //MARK - Tableview Datasource Methods
